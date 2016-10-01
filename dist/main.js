aee0acfd7a75f2a903101f4ab34295183414f9a8
Ali Hamud <alihamud@microsoft.com>
2016-10-01 23:02:24 +0300

    first typescript checkin first submit.

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+"use strict";
+function years(birthdayYear) {
+    return 2016 - birthdayYear;
+}
+exports.years = years;
+console.log("Years " + years(1989));
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+export function years(birthdayYear: number) {
+    return 2016 - birthdayYear;
+}
